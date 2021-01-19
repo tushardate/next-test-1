da7510c9f3aaf1eec451f7878f622eacfd52ebd8
Tushar Date <tushardate@gmail.com>
2021-01-19 14:55:27 -0800

    updated carousel colors

--- components/Carousel.js
+++ components/Carousel.js
@@ -9,8 +9,8 @@ function Carousel(props) {
   props.content_carousel.forEach((el, i) => {
     return slides.push(
       <SwiperSlide key={i}>
-        <div className="w-full h-full flex justify-center">
-          <img className="h-full object-scale-down" src={el.url}></img>
+        <div className="w-full h-full flex justify-center carouselHeight">
+          <img className="w-full object-scale-down" src={el.url}></img>
         </div>
       </SwiperSlide>
     );
@@ -18,9 +18,8 @@ function Carousel(props) {
 
   return (
     <>
-      <p className="text-5xl py-32">Carousel</p>
       <div className={props.carousel_item_classes}>
-        <Swiper id="main" className="w-full h-full pb-10" autoplay={{delay: 4000}} navigation pagination centeredSlides spaceBetween={24}>{slides}</Swiper>
+        <Swiper id="main" className="w-full h-full pb-6" autoplay={{delay: 4000}} navigation pagination centeredSlides spaceBetween={12} slidesPerView={2}>{slides}</Swiper>
       </div>
     </>
   );
